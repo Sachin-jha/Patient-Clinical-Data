d0598b22be68e0ab28aea5df975dc2e35e9a38d7
Sachin-jha <sendtosachin27@gmail.com>
2020-06-14 15:34:47 +0530

    Updates

---
+++ precache-manifest.bd31ef807f8581bf0813abc7cfff1ddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d8f3cc4443a04462254998699d9730",
+    "url": "/Patient-Clinical-Data/index.html"
+  },
+  {
+    "revision": "28dfad62473989d6dc53",
+    "url": "/Patient-Clinical-Data/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "7b5b118717f46b3874a7",
+    "url": "/Patient-Clinical-Data/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28dfad62473989d6dc53",
+    "url": "/Patient-Clinical-Data/static/js/2.af3331bf.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/Patient-Clinical-Data/static/js/2.af3331bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5b118717f46b3874a7",
+    "url": "/Patient-Clinical-Data/static/js/main.1a50eec3.chunk.js"
+  },
+  {
+    "revision": "4e8b8cd41b3b2cf55b52",
+    "url": "/Patient-Clinical-Data/static/js/runtime-main.060b36f0.js"
+  }
+]);
